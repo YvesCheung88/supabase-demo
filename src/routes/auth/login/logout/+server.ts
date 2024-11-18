45e0ecb698a80c2d60f0053641d6a8f49b6e8a21
YvesCheung88 <yves.cheung.88@gmail.com>
2024-11-18 14:11:43 +0100

    fixed build prerender

--- src/routes/auth/login/logout/+server.ts
+++
@@ -1,9 +0,0 @@
-// src/routes/auth/logout.ts
-import { redirect } from "@sveltejs/kit";
-import { supabase } from "$lib/supabase";
-
-export const GET = async () => {
-  await supabase.auth.signOut();
-
-  throw redirect(303, "/auth/login");
-};
