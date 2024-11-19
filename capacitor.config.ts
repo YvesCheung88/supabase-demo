618f0641d84494bab442fc9bb46072c20db28914
YvesCheung88 <yves.cheung.88@gmail.com>
2024-11-19 10:27:24 +0100

    capacitor added with android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.example.app",
+  appName: "supabase-demo",
+  webDir: "build",
+};
+
+export default config;
